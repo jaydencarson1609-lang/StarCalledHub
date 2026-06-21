4f1c462a059db8dffc914fd1d4a2e73498867dca
jaydencarson1609-lang <jaydencarson1609@gmail.com>
2026-06-21 01:43:35 +0100

    Create Tower.lua for cobblestone tower structure
    Added a Lua script to create a simple cobblestone tower structure in the workspace.

--- Builds/Tower.lua
+++ Builds/Tower.lua
@@ -1,1 +1,35 @@
-
+-- Tower.lua - Simple Cobblestone Tower
+return {
+    -- Base layer
+    {"Cobblestone", CFrame.new(0, 2, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 2, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 2, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 2, 4), workspace.Baseplate},
+    
+    -- Walls up to height 20
+    {"Cobblestone", CFrame.new(0, 6, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 6, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 6, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 6, 4), workspace.Baseplate},
+    
+    {"Cobblestone", CFrame.new(0, 10, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 10, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 10, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 10, 4), workspace.Baseplate},
+    
+    {"Cobblestone", CFrame.new(0, 14, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 14, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 14, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 14, 4), workspace.Baseplate},
+    
+    {"Cobblestone", CFrame.new(0, 18, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 18, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 18, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 18, 4), workspace.Baseplate},
+    
+    -- Top platform
+    {"Cobblestone", CFrame.new(0, 22, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 22, 0), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(0, 22, 4), workspace.Baseplate},
+    {"Cobblestone", CFrame.new(4, 22, 4), workspace.Baseplate},
+}
